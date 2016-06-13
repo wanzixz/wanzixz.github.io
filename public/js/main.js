ca78c2b641d9c8a09d542d1bd956ddda097fad41
wanzixz <1471299010@qq.com>
2016-06-13 20:53:53 +0800

    Update main.js
    js

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ var afterPjax = function() {
       ds.type = 'text/javascript';ds.async = true;
       ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
       ds.charset = 'UTF-8';
-      (document.getElementsByTagName('head')[0] 
+      (main 
        || document.getElementsByTagName('body')[0]).appendChild(ds);
     })();
   };
