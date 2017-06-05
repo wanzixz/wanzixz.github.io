661efb58b1a189e924a41b789ca6d32d69567df5
LZhang <Lzhang@snsshop.net>
2017-06-05 09:55:05 +0800

    weinimei

---
+++ project/weinimei/js/main.js
@@ -0,0 +1,41 @@
+$(function(){
+
+$('.dropdown-toggle').dropdownHover();
+
+//首页banner
+var mySwiper = new Swiper ('.banner', {
+    autoplay: 5000,
+    loop: true,
+     paginationClickable: true,
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+    
+    // 如果需要前进后退按钮
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    
+  });  
+
+//买家秀
+var swiper = new Swiper('#js-home-show', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 30,
+        freeMode: true,
+        // 如果需要前进后退按钮
+	    nextButton: '.swiper-button-next',
+	    prevButton: '.swiper-button-prev',
+    });
+
+//滚动加载
+var wow = new WOW(
+  {
+    animateClass: 'animated',
+    offset:100
+  }
+);
+wow.init();
+
+
+})
